dfb033ed49d4a3b0d168bf110d4229f7fa4b23dc
Tim Engle <timothy.englew@gmail.com>
2023-06-22 11:51:50 -0700

    Set random product object to state on page load

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
+import axios from "axios";
+import getRandomProd from "../helperFunctions/App/getRandomProd.js";
 import Overview from "./Overview/Overview.jsx";
 import ItemsComponent from "./ItemsComponent/ItemsComponent.jsx";
 import QuesAnswer from "./QuesAnswer/QuesAnswer.jsx";
@@ -6,10 +8,13 @@ import RatingReview from "./RatingReview/RatingReview.jsx";
 
 const App = () => {
   const apiKey = process.env.REACT_APP_API_KEY;
-  const [currItem, setCurrItem] = useState([]);
+  const [currItem, setCurrItem] = useState({});
+  useEffect(() => {
+    getRandomProd().then((data) => {
+      setCurrItem(data);
+    });
+  }, []);
 
-
-  console.log(apiKey)
   return (
     <div>
       <h1>Hello worlds!</h1>
