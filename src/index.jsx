45bbb3053a8bd57e8c7d913908330629a1edb526
Tim Engle <timothy.englew@gmail.com>
2023-06-22 10:33:36 -0700

    Fix depricated issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./components/App.jsx";
 
-const domNode = document.getElementById("root");
-render(<App />, domNode);
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
