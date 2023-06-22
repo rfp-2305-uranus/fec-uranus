a9f64f93457bd251e79fddbb0037afbbd516ef4f
Tim Engle <timothy.englew@gmail.com>
2023-06-22 16:52:24 -0700

    Added function to get styles from API based on product ID Converted both functions to use async await format

--- src/helperFunctions/App/getRandomProd.js
+++ src/helperFunctions/App/getRandomProd.js
@@ -1,16 +1,17 @@
 import axios from "axios";
-
 const apiKey = process.env.REACT_APP_API_KEY;
 
-
-const getRandomProd = () => {
-  return axios({
-    method: "GET",
-    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/?count=100`,
-    headers: { Authorization: apiKey },
-  }).then((data) => {
-    return data.data[Math.floor(Math.random() * (99 - 0) + 0)];
-  });
+const getRandomProd = async () => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/?count=100`,
+      headers: { Authorization: apiKey },
+    });
+    return response.data[Math.floor(Math.random() * (99 - 0) + 0)];
+  } catch (err) {
+    return err;
+  }
 };
 
-export default getRandomProd
+export default getRandomProd;
