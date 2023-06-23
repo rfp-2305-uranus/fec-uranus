bc310558b603c158900c9270b6537c23ae1ce469
Loren <loren.johnson99@yahoo.com>
2023-06-23 12:24:50 -0700

    includes helper function to get review meta data by product id

--- src/components/RatingReview/RatingReview.jsx
+++ src/components/RatingReview/RatingReview.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import ReviewsList from './ReviewsListModule/ReviewsList.jsx'
 import RatingBreakdown from './RatingBreakdownModule/RatingBreakdown.jsx'
 import ProductBreakdown from './ProductBreakdownModule/ProductBreakdown.jsx'
 import WriteReview from './WriteReviewModule/WriteReview.jsx'
 
+import getReviewMetadata from '../../helperFunctions/getReviewMetadata.js'
+
 const RatingReview = ({ currItem }) => {
   console.log(currItem);
-  // THIS IS A TEST
+  // currItem properties:
+  // category, default_price, description, id, name, slogan, created_at, updated_at
+
   // make request to API for review data
+  useEffect(() => {
+    getReviewMetadata();
+  }, [])
 
   return <section>
     <ReviewsList />
---
+++ src/helperFunctions/getReviewMetadata.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const getReviewMetadata = (id) => {
+  console.log(`getReviewMetadata helper called!`);
+};
+
+export default getReviewMetadata;
