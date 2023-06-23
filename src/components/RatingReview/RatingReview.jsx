b2507610b43617b0e225cf262f3f0e6f3a5a1810
Loren <loren.johnson99@yahoo.com>
2023-06-23 15:42:47 -0700

    declares states for review data (list of reviews, current page) and product data (characteristics, ratings, recommended)

--- src/components/RatingReview/RatingReview.jsx
+++ src/components/RatingReview/RatingReview.jsx
@@ -10,15 +10,22 @@ import getReviewMetadata from '../../helperFunctions/getReviewMetadata.js'
 import getReviews from '../../helperFunctions/getReviews.js'
 
 const RatingReview = ({ currItem }) => {
-  console.log(currItem);
-  // currItem properties: category, default_price, description, id, name, slogan, created_at, updated_at
+  const [reviews, setReviews] = useState([]);
+  const [page, setPage] = useState(1);  // API set to return 2 reviews per page
+  const [characteristics, setCharacteristics] = useState({});
+  const [ratings, setRatings] = useState({});
+  const [recommended, setRecommended] = useState({});
 
   // make request to API for reviews, metadata
   useEffect(() => {
     async function getReviewData() {
       try {
         const metadata = await getReviewMetadata(currItem.id);
-        const reviews = await getReviews(currItem.id);
+        // get reviews sorted by relevance, start at page 1, only 2 reviews per page
+        const reviews = await getReviews(currItem.id, 'relevant', 1, 2);
+        setReviews(reviews);
+        console.log(reviews);
+        console.log(metadata);
         // SET STATES
       } catch (error) {
         console.log(error);
