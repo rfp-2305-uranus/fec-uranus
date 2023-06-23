703451f516c9a43b0115e6874f955d4a46fc11f4
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-06-23 16:25:14 -0700

    created a helpfucn for product and created a obj of use for my module

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,14 @@
-import React, { Component, useEffect, useState } from "react";
-import axios from "axios";
-import getRandomProd from "../helperFunctions/App/getRandomProd.js";
-import Overview from "./Overview/Overview.jsx";
-import ItemsComponent from "./ItemsComponent/ItemsComponent.jsx";
-import QuesAnswer from "./QuesAnswer/QuesAnswer.jsx";
-import RatingReview from "./RatingReview/RatingReview.jsx";
+import React, { Component, useEffect, useState } from 'react';
+import axios from 'axios';
+import getRandomProd from '../helperFunctions/App/getRandomProd.js';
+import Overview from './Overview/Overview.jsx';
+import ItemsComponent from './ItemsComponent/ItemsComponent.jsx';
+import QuesAnswer from './QuesAnswer/QuesAnswer.jsx';
+import RatingReview from './RatingReview/RatingReview.jsx';
 
-import "./App.css";
+import './App.css';
 
-const App = () => {
+function App() {
   const apiKey = process.env.REACT_APP_API_KEY;
   const [currItem, setCurrItem] = useState(null);
   useEffect(() => {
@@ -24,12 +24,12 @@ const App = () => {
   return (
     <div className="app-container">
       <h1>Hello worlds!</h1>
-      <Overview currItem = {currItem} />
-      <ItemsComponent currItem = {currItem} />
+      <Overview currItem={currItem} />
+      <ItemsComponent currItem={currItem} />
       <QuesAnswer />
       <RatingReview />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,23 +1,59 @@
-import React from "react";
+/* eslint-disable camelcase */
+/* eslint-disable react/function-component-definition */
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useRef, useState } from 'react';
+import axios from 'axios';
+import getStylesById from '../../helperFunctions/App/getStylesById.js';
+import getProductById from '../../helperFunctions/App/getProductById.js';
 
-const Overview = ({currItem}) => {
-  console.log(currItem);
+const apiKey = process.env.REACT_APP_API_KEY;
 
-  // create a use effect to
+const Overview = ({ currItem }) => {
+  const mainObj = useRef({});
+  useEffect(() => {
+    let obj = {};
+    getStylesById(currItem.id)
+      .then((response) => {
+        obj.styles = response.results;
+      })
+      .then(() => getProductById(currItem.id))
+      .then((response) => {
+        const {
+          // eslint-disable-next-line camelcase
+          id, name, slogan, description, category, default_price,
+        } = response;
+        obj = {
+          ...obj, id, name, slogan, defaultPrice: default_price, description, category,
+        };
+      })
+      .then(() => axios.get(
+        `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/meta/?product_id=${currItem.id}`,
+        {
+          headers: { Authorization: apiKey },
+        },
+      ))
+      .then(({ data }) => {
+        obj.ratings = data.ratings;
+        mainObj.current = obj;
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, []);
+
   return (
-    <section className = "overview-section">
-    <div className = "promotion-container">Current Promotion Container</div>
-    <div className = "product-container">
-      Image Gallery and Product Overview Container
-      <div className = "image-gallery-container">Image Gallery</div>
-      <div className = "product-details-container">Product Overview</div>
-    </div>
-    <div className = "description-container">
-      Description container
-    </div>
-  </section>
-  )
+    <section className="overview-section">
+      <div className="promotion-container">Current Promotion Container</div>
+      <div className="product-container">
+        Image Gallery and Product Overview Container
+        <div className="image-gallery-container">Image Gallery</div>
+        <div className="product-details-container">Product Overview</div>
+      </div>
+      <div className="description-container">
+        Description container
+      </div>
+    </section>
+  );
 };
 
 export default Overview;
-
