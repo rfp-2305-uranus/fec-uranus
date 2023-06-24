c175b0f7d2465615f25dc926ee16a55c1ccd0191
Loren <loren.johnson99@yahoo.com>
2023-06-24 10:14:49 -0700

    renders seller response if present

--- src/components/RatingReview/ReviewsListModule/ReviewTile.jsx
+++ src/components/RatingReview/ReviewsListModule/ReviewTile.jsx
@@ -3,35 +3,43 @@ import StarRating from '../../Utilities/StarRating.jsx';
 
 const ReviewTile = ({ review }) => {
   const { body, date, helpfulness, photos, rating, recommend, response, review_id, reviewer_name, summary } = review;
-  console.log(body);
   console.log(review);
 
   // if review body is longer than char limit, show button and limit chars displayed
   const charLimit = 50; // **should be 250 for final product
   const [showButton, setShowButton] = useState(
-    (body.length > charLimit) ? true : false);
-  const [bodyDisplay, setBodyDisplay] = useState(
-    (body.length > charLimit) ? body.slice(0, charLimit) : body);
+    (body.length > charLimit) ? true : false
+    );
+  const [reviewDisplay, setReviewDisplay] = useState(
+    (body.length > charLimit) ? body.slice(0, charLimit) : body
+    );
 
-  const [isVerified, setIsVerified] = useState(false); // TODO: check if user email is associated with sale in system
+  // render images if provided
+  const [images, setImages] = useState(
+    (photos.length) ? true : false
+  );
 
+  // TODO: check if user email is associated with sale in system
+  const [isVerified, setIsVerified] = useState(false);
 
   // was this review helpful - yes/no radio buttions
+
   // render thumbnails if images
 
   return (
     <div className='reviewTile'>
+      --------------------------------
       <h3>{StarRating({rating})}</h3>
 
       <div className='reviewDate'>{date}</div>
 
       <div className='reviewSummary'>{summary}</div>
 
-      <div className='reviewBody'>{bodyDisplay}</div>
+      <div className='reviewBody'>{reviewDisplay}</div>
       {/* button only shows if body is longer than 250 */}
       {showButton && <button onClick={() => {
         setShowButton(false)
-        setBodyDisplay(body)
+        setReviewDisplay(body)
         }}>Show more</button>}
 
       <div className='reviewImages'>images</div>
@@ -42,8 +50,14 @@ const ReviewTile = ({ review }) => {
       </div>
 
       {recommend && <div>I recommend this product</div>}
-      response from seller
-      helpfullness button
+      {response && <div>
+        <h5>Response from Seller</h5>
+        {response}
+      </div>}
+      <div className='reviewHelpfulness'>
+        yes/no buttons
+      </div>
+      --------------------------------
     </div>
   );
 };
