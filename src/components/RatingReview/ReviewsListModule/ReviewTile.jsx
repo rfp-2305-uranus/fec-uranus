ec334efec305150c7df66fc2bf5329ddcbaa7cdb
Loren <loren.johnson99@yahoo.com>
2023-06-23 18:21:59 -0700

    shows star ratings in each review tile

--- src/components/RatingReview/ReviewsListModule/ReviewTile.jsx
+++ src/components/RatingReview/ReviewsListModule/ReviewTile.jsx
@@ -1,15 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import StarRating from '../../Utilities/StarRating.jsx';
 
-const ReviewTile = ({review}) => {
+const ReviewTile = ({ review }) => {
   const { body, date, helpfulness, photos, rating, recommend, response, review_id, reviewer_name, summary } = review;
   console.log(body);
   console.log(review);
 
-  // if review body is longer than 250 char, truncate body in preview to 250
-  const [truncateBody, setTruncateBody] = useState(
-    (body.length > 50) ? true : false);
+  // if review body is longer than char limit, show button and limit chars displayed
+  const charLimit = 50; // **should be 250 for final product
+  const [showButton, setShowButton] = useState(
+    (body.length > charLimit) ? true : false);
   const [bodyDisplay, setBodyDisplay] = useState(
-    (body.length > 50) ? body.slice(0, 50) : body);
+    (body.length > charLimit) ? body.slice(0, charLimit) : body);
 
   const [isVerified, setIsVerified] = useState(false); // TODO: check if user email is associated with sale in system
 
@@ -17,25 +19,33 @@ const ReviewTile = ({review}) => {
   // was this review helpful - yes/no radio buttions
   // render thumbnails if images
 
-  return <div className='reviewTile'>
-    <h3>Star rating</h3>
-    <div className='reviewDate'>{date}</div>
-    <div className='reviewSummary'>{summary}</div>
-    <div className='reviewBody'>{bodyDisplay}</div>
-    {truncateBody && <button onClick={() => {
-      setTruncateBody(false)
-      setBodyDisplay(body)
-      }}>Show more</button>}
-    <div>images</div>
-    <div className='reviewerName'>
-      {reviewer_name}
-      {isVerified && <span> Verified Purchaser</span>}
-    </div>
+  return (
+    <div className='reviewTile'>
+      <h3>{StarRating({rating})}</h3>
 
-    {recommend && <div>I recommend this product</div>}
-    response from seller
-    helpfullness button
-  </div>
-}
+      <div className='reviewDate'>{date}</div>
+
+      <div className='reviewSummary'>{summary}</div>
+
+      <div className='reviewBody'>{bodyDisplay}</div>
+      {/* button only shows if body is longer than 250 */}
+      {showButton && <button onClick={() => {
+        setShowButton(false)
+        setBodyDisplay(body)
+        }}>Show more</button>}
+
+      <div className='reviewImages'>images</div>
+
+      <div className='reviewerName'>
+        {reviewer_name}
+        {isVerified && <span> Verified Purchaser</span>}
+      </div>
+
+      {recommend && <div>I recommend this product</div>}
+      response from seller
+      helpfullness button
+    </div>
+  );
+};
 
 export default ReviewTile;
