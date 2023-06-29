fe0e1098127d152bd92bab30326e0b20abec994e
Loren <loren.johnson99@yahoo.com>
2023-06-29 11:48:41 -0700

    adds test dependencies to package.json, babel.config.js, and a passing unit test

--- src/components/RatingReview/ReviewsListModule/ReviewHelpfulness.test.js
+++ src/components/RatingReview/ReviewsListModule/ReviewHelpfulness.test.js
@@ -1,6 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+// tells jest to mock document and window
+
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import ReviewHelpfulness from './ReviewHelpfulness.jsx';
+import ReviewHelpfulness from './ReviewHelpfulness';
 // takes prop reviewHelpfulness (number) and
 test('increments helpfuless score on button click', () => {
-  render()
+  const reviewHelpfulness = 12;
+  render(<ReviewHelpfulness reviewHelpfulness={reviewHelpfulness} />)
+  const helpfulElement = screen.getByRole('button');
+  expect(helpfulElement).toBeInTheDocument();
+
 });
