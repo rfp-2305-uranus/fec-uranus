f67ff3f2d889742779d4250212e7563b574072a0
Loren <loren.johnson99@yahoo.com>
2023-06-29 12:19:31 -0700

    adds outline for future unit test

--- src/components/RatingReview/ReviewsListModule/ReviewHelpfulness.test.js
+++ src/components/RatingReview/ReviewsListModule/ReviewHelpfulness.test.js
@@ -4,7 +4,7 @@
 // tells jest to mock document and window
 
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ReviewHelpfulness from './ReviewHelpfulness';
 
 it('displays helpfulness prop in button', () => {
@@ -12,3 +12,16 @@ it('displays helpfulness prop in button', () => {
   const helpfulElement = screen.getByRole('button', {name: 'Yes (12)'});
   expect(helpfulElement).toBeInTheDocument();
 });
+
+
+// TODO: Write test to check that helpfulness count increments on button click
+
+// it('increments helpfulness count on button click', () => {
+//   render(<ReviewHelpfulness reviewHelpfulness={12} />)
+//   // ReviewHelpfulness accepts event handler to update count **
+//   const helpfulElement = screen.getByRole('button', {name: 'Yes (12)'});
+//   fireEvent.click(helpfulElement);
+//   helpfulElement = screen.getByRole('button', {name: 'Yes (13)'});
+//   expect(helpfulElement).toBeInTheDocument();
+// });
+
