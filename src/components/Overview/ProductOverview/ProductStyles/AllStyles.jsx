6219b065966361465a5b2ec72ca2351ffb0931ca
Tim Engle <timothy.englew@gmail.com>
2023-06-29 18:31:49 -0700

    Comment out log

--- src/components/Overview/ProductOverview/ProductStyles/AllStyles.jsx
+++ src/components/Overview/ProductOverview/ProductStyles/AllStyles.jsx
@@ -3,34 +3,36 @@ import { useState, useEffect } from 'react';
 import IndividualStyleComponent from './IndividualStyleComponent.jsx';
 import './ProductStylesCSS/styles.css';
 
-const AllStyles = ({ styles, setCurrStyle, setOnSale}) => {
+const AllStyles = ({ styles, setCurrStyle, setOnSale }) => {
   const [isSelected, setIsSelected] = useState(styles[0]);
   useEffect(() => {
-    setIsSelected(styles[0])
-  }, [styles])
-  console.log('ALL STYLES', styles);
-  console.log('SELECTED', isSelected);
+    setIsSelected(styles[0]);
+  }, [styles]);
+  // console.log('ALL STYLES', styles);
+  // console.log('SELECTED', isSelected);
   const onStyleHandler = (style) => {
     setCurrStyle(style);
     setIsSelected(style);
     style.sale_price ? setOnSale(true) : setOnSale(false);
-  }
-  if(styles) {
+  };
+  if (styles) {
     return (
-      <div className="all-styles-container" >
+      <div className="all-styles-container">
         {styles.map((style) => {
-          return<IndividualStyleComponent
-            key={style.style_id}
-            style={style}
-            onStyleHandler={onStyleHandler}
-            isSelected={isSelected}
-          />
+          return (
+            <IndividualStyleComponent
+              key={style.style_id}
+              style={style}
+              onStyleHandler={onStyleHandler}
+              isSelected={isSelected}
+            />
+          );
         })}
       </div>
     );
   }
   return null;
-}
+};
 export default AllStyles;
 
 //style={{display:'flex', felxDirection: 'row', gap:'0.7rem'}}
