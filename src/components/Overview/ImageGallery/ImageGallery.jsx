786a702b76a0cf83f8b80b99766f3d5fd3d5916f
Girijesh Thodupunuri <girinuri123@gmail.com>
2023-07-01 23:25:05 -0700

    Finished scroll feature for image thumbnails

--- src/components/Overview/ImageGallery/ImageGallery.jsx
+++ src/components/Overview/ImageGallery/ImageGallery.jsx
@@ -1,6 +1,7 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import ThumbNailImage from './ThumbNailImages/ThumbNailImages.jsx';
 import {dummyData} from './dummyData.js';
+import {AiOutlineArrowDown, AiOutlineArrowUp} from 'react-icons/ai';
 import './imageGallery.css'
 
     const dummyImages = dummyData.results.map(({photos}) => {
@@ -11,50 +12,73 @@ import './imageGallery.css'
 
 
 const ImageGallery = ({currItem, currStyles, currentStyle, setCurrentStyle}) => {
-  const [thumbNailImages, setThumbNailImages] = useState(dummyImages);
+  const [thumbNailImages, setThumbNailImages] = useState(null);
   const [currMainImage, setCurrMainImage] = useState(null);
-  const [isSelected, setIsSelected] = useState(null)
+  const [isSelected, setIsSelected] = useState(null);
+  const thumbNailContainer = useRef(null);
   // Main img is diff from thumbnail imgs
 
 
 
-  // // useEffect(()=> {
-  // //   if(currStyles) {
-  // //     console.log(currStyles);
+  useEffect(()=> {
+    if(currStyles) {
 
-  // //     const images = currStyles.results.map(({photos}) => {
-  // //       const obj = photos[0]; // as the object of images
-  // //       const{url} = obj;
-  // //       return url;
-  // //     })
-  // //     setThumbNailImages(images);
-  // //   }
-  // // }, [currStyles])
+      const images = currStyles.results.map(({photos}) => {
+        const obj = photos[0]; // as the object of images
+        const{url} = obj;
+        return url;
+      })
+      setThumbNailImages(images);
+    }
+  }, [currStyles])
 
   useEffect(() => {
-    console.log('MAIN IMAGE', currentStyle.photos[0].url)
     setCurrMainImage(currentStyle.photos[0].url);
     setIsSelected(currentStyle.photos[0].url);
   }, [currentStyle])
+
+  const onUpClickHandler = () => {
+    const amountToScrollBy= thumbNailContainer.current.scrollTop - 80;
+    thumbNailContainer.current.scrollTo({
+      top: amountToScrollBy,
+      behavior: 'smooth'
+    })
+  }
+
+  const onDownClickHandler = () => {
+    const amountToScrollBy = thumbNailContainer.current.scrollTop + 80;
+    // scrollTop give the distance between top of container to current position
+    thumbNailContainer.current.scrollTo({
+      top: amountToScrollBy,
+      behavior: 'smooth'
+    })
+  }
 
   const onThumbnailImageHandler = (image) =>{
     setCurrMainImage(image);
     setIsSelected(image);
   }
   if(thumbNailImages && currMainImage) {
-    console.log('STATE', currMainImage);
+
     return (
       <div className="image-gallery-container" >
-        <div className="thumbnail-images-container">
-          {thumbNailImages.map((image) => {
-            return(
-              <ThumbNailImage
-                onThumbnailImageHandler ={onThumbnailImageHandler}
-                image ={image}
-                isSelected={isSelected}
-              />
-              )
-          })}
+        <div className = "arrows-plus-thumbnails">
+          {thumbNailImages.length >=7 && <AiOutlineArrowUp onClick = {onUpClickHandler} /> }
+
+          <div className="thumbnail-images-container" ref = {thumbNailContainer}>
+            {thumbNailImages.map((image, index) => {
+              console.log('Key', `${image}-${index}`)
+              return(
+                <ThumbNailImage
+                  onThumbnailImageHandler ={onThumbnailImageHandler}
+                  image ={image}
+                  isSelected={isSelected}
+                  key = {`${image}-${index}`}
+                />
+                )
+            })}
+          </div>
+          {thumbNailImages.length >=7 &&<AiOutlineArrowDown onClick = {onDownClickHandler}/>}
         </div>
         <div className="main-image-container" >
           <img src ={currMainImage} className="main-image" />
